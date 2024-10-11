f93162bbc08d4f1b345cedeed135b4d958abad9a
Andres <andres.torressandovalhotmail.com>
2024-10-11 15:54:05 -0500

    Section-14-2

---
+++ src/components/Layout/TopBar/TopBar.tsx
@@ -0,0 +1,27 @@
+import { Image } from "semantic-ui-react";
+import styles from "./TopBar.module.scss";
+import Link from "next/link";
+
+
+
+export function TopBar(props: any) {
+    const { isOpenSearch } = props;
+
+    return (
+        <div className={styles.topBar}>
+            <div className={styles.left}>
+                <Link href="/">
+                    <Image src="/images/logo.png" alt="Gaming" />
+                </Link>
+            </div>
+
+            <div className={styles.center}>
+                <span>MENU</span>
+            </div>
+
+            <div className={styles.right}>
+                <span>ACCOUNT</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/layouts/BasicLayout/BasicLayout.tsx
@@ -0,0 +1,36 @@
+import styles from "./BasicLayout.module.scss"
+import { Container } from "semantic-ui-react";
+import classNames from "classnames";
+import { TopBar } from "@/components/Layout";
+
+type PropsTypes = {
+    children: React.ReactNode
+        isOpenSearch: boolean
+        isContainer: boolean
+        relative: boolean
+}
+
+
+export function BasicLayout(props: PropsTypes) {
+
+
+    const { children,
+        isOpenSearch = false,
+        isContainer = false,
+        relative = false,
+    } = props;
+
+    return (
+        <>
+           
+           <TopBar isOpenSearch={isOpenSearch}/>
+            <Container fluid>
+                <div className={classNames({ [styles.relative]: relative })}>
+                    {isContainer ? <Container>{children}</Container> : children}
+                </div>
+            </Container>
+
+            {/* TODO: Footer*/}
+        </>
+    );
+}
