7d1bf561fe5a13f57dc729cc545b8d92ca82cc91
Andres <andres.torressandovalhotmail.com>
2024-10-24 20:44:17 -0500

    section 15: 103 - update user email

--- src/components/Account/Settings/ChangeEmailForm/ChangeEmailForm.tsx
+++ src/components/Account/Settings/ChangeEmailForm/ChangeEmailForm.tsx
@@ -1,18 +1,24 @@
 import { Form } from 'semantic-ui-react'
 import { useFormik } from 'formik';
 import { initialValues, validationSchema } from './ChangeEmailForm.form'
+import { User } from '@/api';
+import { useAuth } from '@/hooks';
 import styles from './ChangeEmailForm.module.scss';
+
+const userCtrl = new User()
 
 export function ChangeEmailForm() {
 
+    const { user, updateUser } = useAuth()
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: validationSchema(),
         validateOnChange: false,
         onSubmit: async (formValue) => {
             try {
-                console.log('send form');
-                console.log(formValue);
+                await userCtrl.updateMe(user?.id, { email: formValue.email })
+                updateUser("email", formValue.email)
+                formik.handleReset();
             } catch (error) {
                 console.log(error);
 
