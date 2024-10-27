5fa8f79241d888749fb5b589a82b868a9f2f7cd1
Andres <andres.torressandovalhotmail.com>
2024-10-26 22:48:50 -0500

    Section 16: 110 Created modal

--- src/components/Account/Address/AddAddress/AddAddress.tsx
+++ src/components/Account/Address/AddAddress/AddAddress.tsx
@@ -1,21 +1,26 @@
 import { useState } from 'react'
 import styles from './AddAddress.module.scss'
 import { Button } from 'semantic-ui-react';
+import { BasicModal } from '@/components/Shared';
 
 export function AddAddress() {
 
     const [show, setShow] = useState(false);
 
-        const onOpenClose = () => setShow((prevState) => !prevState)
+    const onOpenClose = () => setShow((prevState) => !prevState)
     return (
 
         <>
-            <Button 
-            primary 
-            className={styles.addBtn} 
-            onClick={onOpenClose}>
+            <Button
+                primary
+                className={styles.addBtn}
+                onClick={onOpenClose}>
                 Create
             </Button>
+
+            <BasicModal show={show} onClose={onOpenClose} title='New Address'>
+                <h2>Contenido del basic modal</h2>
+            </BasicModal>
         </>
     )
 }
---
+++ src/components/Shared/BasicModal/BasicModal.tsx
@@ -0,0 +1,22 @@
+import { Modal } from 'semantic-ui-react';
+import style from './BasicModal.module.scss';
+
+type MyComponentProps = {
+    children: React.ReactNode,
+    show: boolean,
+    onClose: ()=>void,
+    title: string
+};
+
+export function BasicModal({ children, show, onClose, title }: MyComponentProps) {
+
+
+    return (
+        <Modal open={show} onClose={onClose} size="small">
+            <Modal.Header>{title}</Modal.Header>
+            <Modal.Content>
+                {children}
+            </Modal.Content>
+        </Modal>
+    );
+}
