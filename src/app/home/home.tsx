fa8a0b48ae87b27574dec11b1ca4daec56fde44b
Andres <andres.torressandovalhotmail.com>
2024-11-01 20:41:10 -0500

    Section 17: 126 last playstation games

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -4,6 +4,13 @@ import { BarTrust, Separator } from "@/components/Shared";
 import { Container } from "semantic-ui-react";
 
 
+const platformsId = {
+    playStation: 1,
+    xbox: 4,
+    nintendo: 2,
+    pc: 3,
+}
+
 export default function HomePage() {
     return (
         <BasicLayout>
@@ -15,6 +22,15 @@ export default function HomePage() {
             <Separator height={100} />
             <BarTrust />
             <Separator height={100} />
+            <Container>
+                <Home.LatestGames
+                    title='playStation'
+                    limit={3}
+                    platformId={platformsId.pc}
+                />
+            </Container>
+            <Separator height={100} />
+
         </BasicLayout>
     );
 }
