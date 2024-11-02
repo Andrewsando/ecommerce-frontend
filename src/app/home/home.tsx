84791620083f40b584eee02951ea78e44b91c125
Andres <andres.torressandovalhotmail.com>
2024-11-01 20:20:39 -0500

    Section 17: 125 Info bar

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -1,6 +1,6 @@
 import { BasicLayout } from "@/layouts";
 import { Home } from "@/components/Home";
-import { Separator } from "@/components/Shared";
+import { BarTrust, Separator } from "@/components/Shared";
 import { Container } from "semantic-ui-react";
 
 
@@ -12,6 +12,9 @@ export default function HomePage() {
             <Container>
                 <Home.LatestGames title="Last games" />
             </Container>
+            <Separator height={100} />
+            <BarTrust />
+            <Separator height={100} />
         </BasicLayout>
     );
 }
---
+++ src/components/Shared/BarTrust/BarTrust.data.tsx
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        icon: "truck",
+        title: "Super fast",
+        description: "Delivery in 24/8 hours",
+    },    
+    {
+        icon: "shield",
+        title: "Reliable and secure",
+        description: "More than 10.000 games",
+    },   
+     {
+        icon: "wechat",
+        title: "Customer service",
+        description: "Agent available 24/7",
+    },
+];
