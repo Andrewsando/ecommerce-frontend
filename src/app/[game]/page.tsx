bb829bdfa9c1b95f5d52cadcfb96b5bb93385a95
Andres <andres.torressandovalhotmail.com>
2024-11-07 10:20:44 -0500

    Section 20: Game page created

---
+++ src/app/[game]/page.tsx
@@ -0,0 +1,12 @@
+import { BasicLayout } from "@/layouts";
+
+export default function Game(){
+    return (
+        <>
+        <BasicLayout>
+            <h2>Game page</h2>
+        </BasicLayout>
+        </>
+
+    )
+}
