cd9d945f5618a0d4d6492844914216f361440856
Andres <andres.torressandovalhotmail.com>
2024-11-08 10:04:21 -0500

    Section 20: 140 game panel structure

--- src/app/[game]/page.tsx
+++ src/app/[game]/page.tsx
@@ -1,5 +1,6 @@
 import { Game as GameClass } from "@/api";
 import { Game as GameComponent } from "@/components/Game";
+import { Separator } from "@/components/Shared";
 import { BasicLayout } from "@/layouts";
 
 export default async function Game({ params, searchParams }:
@@ -9,15 +10,20 @@ export default async function Game({ params, searchParams }:
     }) {
     const { game } = params
 
+    
+
     const gameCtrl = new GameClass()
     const response = await gameCtrl.getBySlug(game)
-    console.log('response',response.attributes.platform.data.attributes.icon.data.attributes.url);
+    // console.log('gameee',response);
+    // console.log('response',response.attributes.platform.data.attributes.icon.data.attributes.url);
         const wallpaper = response.attributes.wallpaper.data.attributes.url
 
     return (
         <>
         <BasicLayout>
             <GameComponent.HeaderWallpaper image={wallpaper} />
+            <GameComponent.Panel gameId={response.id} game={response.attributes}/>
+            <Separator height={50} />
         </BasicLayout>
         </>
 
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,6 +1,8 @@
 import HeaderWallpaper from "./HeaderWallpaper/HeaderWallpaper";
+import Panel from "./Panel/Panel";
 
 
 export const Game = {
     HeaderWallpaper,
+    Panel,
 };
