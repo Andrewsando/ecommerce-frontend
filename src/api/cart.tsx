2820e8afb40462a4b2de2a611e56dd47777e22e4
Andres <andres.torressandovalhotmail.com>
2024-11-13 21:42:06 -0500

    Section 22: 156 Adding more than one product to the cart

--- src/api/cart.tsx
+++ src/api/cart.tsx
@@ -2,13 +2,23 @@ import { ENV } from '@/app/utils/constants';
 
 export class Cart {
     add(gameId: any) {
-        localStorage.setItem(ENV.CART, JSON.stringify([gameId]))
+        const games = this.getAll();
+        const objIndex = games.findIndex((game) => game.id === gameId)
+
+        if (objIndex < 0) {
+            gameId.push({ id: gameId, quantity: 1 })
+        } else {
+            const game = games[objIndex]
+            games[objIndex].quantity = game.quantity + 1;
+
+        }
+        localStorage.setItem(ENV.CART, JSON.stringify(games))
     }
 
-    getAll(){
+    getAll() {
         const response = localStorage.getItem(ENV.CART);
-        
-        if(!response){
+
+        if (!response) {
             return [];
         } else {
             return JSON.parse(response)
