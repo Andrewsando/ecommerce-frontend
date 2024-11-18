aba442431c0a11f9618906f65525709754377ab8
Andres <andres.torressandovalhotmail.com>
2024-11-18 09:35:01 -0500

    Section 22: 168 Addresses

--- src/components/Cart/StepTwo/Addresses/Addresses.tsx
+++ src/components/Cart/StepTwo/Addresses/Addresses.tsx
@@ -2,20 +2,21 @@ import { Address } from '@/api'
 import styles from './Addresses.module.scss'
 import { useEffect, useState } from 'react';
 import { useAuth } from '@/hooks';
+import { map } from 'lodash';
+import classNames from 'classnames';
 
 const addressCtrl = new Address();
 
-export function Addresses() {
+export function Addresses({ addressSelected, setAddressSelected }) {
     const [addresses, setAddresses] = useState(null);
     const { user } = useAuth();
-    console.log({addresses});
-
 
     useEffect(() => {
         (async () => {
             try {
                 const response = await addressCtrl.getAll(user?.id)
-                setAddresses(response)
+                setAddresses(response.data)
+                console.log({ addresses });
             } catch (error) {
                 console.error(error);
             }
@@ -23,6 +24,27 @@ export function Addresses() {
     }, [])
 
     return (
-        <div>Addresses</div>
+        <div className={styles.addresses}>
+            <h2>Address</h2>
+
+            {map(addresses, (direccion: Record<string, any>) => {
+                const { name, title, address, postal_code, state, city } = direccion.attributes
+
+                return (
+                    <div
+                        key={address.id}
+                        className={classNames(styles.address, {
+                            [styles.active]: address.id === addressSelected?.id,
+                        }
+                        )}
+                        onClick={() => setAddressSelected(direccion)}
+                    >
+                        <p>{name} ({title})</p>
+                        <p>{address}, {postal_code}, {state}, {city}</p>
+                    </div>
+
+                )
+            })}
+        </div>
     )
 }
--- src/components/Cart/StepTwo/StepTwo.tsx
+++ src/components/Cart/StepTwo/StepTwo.tsx
@@ -1,20 +1,29 @@
 import { Separator } from '@/components/Shared';
 import styles from './StepTwo.module.scss'
 import { Addresses } from './Addresses';
+import { useState } from 'react';
 
 type StepTwoProps = {
     games: any[] | null;
 }
 
-export function StepTwo({ games }:StepTwoProps) {
-    console.log({ games });
+export function StepTwo({ games }: StepTwoProps) {
+
+    const [addressSelected, setAddressSelected] = useState(null)
+    console.log(addressSelected);
+    
 
     return (
         <div className={styles.stepTwo}>
             <div className={styles.center}>
-                <Addresses />
+                <Addresses
+                    addressSelected={addressSelected}
+                    setAddressSelected={setAddressSelected}
+                />
                 <Separator height={50} />
-                <p>PAYMENT</p>
+                {addressSelected && (
+                    <p>PAYMENT</p>
+                )}
             </div>
 
             <div className={styles.right}>
