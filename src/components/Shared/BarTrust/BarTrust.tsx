66522dce92c833a00ea81e6b5def737f9e4a1568
Andres <andres.torressandovalhotmail.com>
2024-11-22 14:29:41 -0500

    Deploy fixes 7

--- src/components/Shared/BarTrust/BarTrust.tsx
+++ src/components/Shared/BarTrust/BarTrust.tsx
@@ -9,7 +9,7 @@ export function BarTrust() {
             <Container className={styles.content}>
                 {map(data, (item) => (
                     <div className={styles.block} key={item.id}>
-                        <Icon name={item?.icon} />
+                        <Icon name={item?.icon as any} />
                         <div>
                             <h5>{item.title}</h5>
                             <span>{item.description}</span>
